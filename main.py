345fd2b3ca3aad891a5887645cbf32a54e80949f
joaogomes762 <joao.gomes1628l@gmail.com>
2025-06-09 19:42:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 
 @app.route("/avaliar-pronuncia", methods=["POST"])
 def avaliar():
